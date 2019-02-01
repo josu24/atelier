671c1b4ccb1a4933f775b962284d91db10df7782
Joel Sundqvist <joelsundqvist@live.se>
2019-02-01 11:35:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import {
+  ParallaxProvider,
+  Parallax,
+  ParallaxBanner
+} from "react-scroll-parallax";
+import { jsx, css, keyframes } from "@emotion/core";
+import styled from "@emotion/styled";
+import logo from "./logo.svg";
+import start from "./start.jpg";
+import scratch from "./scratch.jpg";
+import "./App.css";
+
+const HeroBanner = ({ image, min, max, children }) => (
+  <div className="hero-container">
+    <Parallax offsetYMin={min} offsetYMax={max} slowerScrollRate>
+      <div
+        className="hero-image"
+        style={{ backgroundImage: `url(${image})` }}
+      />
+    </Parallax>
+    <div className="hero-children">{children}</div>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ParallaxProvider>
+        <HeroBanner min={"-30%"} max={"30%"} image={start}>
+          <h1>Hero Banner with Parallax</h1>
+        </HeroBanner>
+      </ParallaxProvider>
     );
   }
 }
